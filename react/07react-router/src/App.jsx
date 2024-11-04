def4a01ef017626db407d29971f28eb80ab7387b
Mihir Jadhav <jadhavmihir143@gmail.com>
2024-11-05 00:04:04 +0530

    react router dom intorduction and basic uses seen in this project.

---
+++ react/07react-router/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-3xl font-extralight bg-red-900 text-white'> React Router DOM!</div>
+  )
+}
+
+export default App
---
+++ react/07react-router/src/main.jsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/contact' element={<Contact/>} />
+      <Route path='/user' element={<User />} >
+        <Route path=':userid' element={<User/>}/>
+      </Route>
+    </Route>
+  )
+)
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
