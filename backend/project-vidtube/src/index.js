41b57b3ce90278f2fe83f68217fe1e9ff218a7be
Mihir Jadhav <jadhavmihir143@gmail.com>
2024-11-07 00:39:44 +0530

    DB connection like a professional in MERN.

--- backend/project-vidtube/src/index.js
+++ backend/project-vidtube/src/index.js
@@ -1,1 +1,24 @@
-console.log("Namaste! from backend.");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './.env'
+})
+
+const PORT = process.env.PORT || 8001
+
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection error ", err);
+    
+})
+
+
