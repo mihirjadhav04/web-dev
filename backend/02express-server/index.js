8c6fbbfb6d5b38e6d69a9127144e8865df52889b
Mihir Jadhav <jadhavmihir143@gmail.com>
2024-11-06 23:38:33 +0530

    completed express server and now started new backend project with professional structre and organization for file structure.

--- backend/02express-server/index.js
+++ backend/02express-server/index.js
@@ -1,8 +1,30 @@
+import logger from "./logger.js";
+import morgan from "morgan";
 import express from 'express';
+
+const morganFormat = ":method :url :status :response-time ms";
 
 const app = express();
 const port = 3000;
 let registeredStudents = [];
+
+
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
+
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
