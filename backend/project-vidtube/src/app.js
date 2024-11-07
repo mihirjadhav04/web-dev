17575e9f3bfeedf61a5a9fade00e3a75d1627ce6
Mihir Jadhav <jadhavmihir143@gmail.com>
2024-11-07 16:12:40 +0530

    created models for our project with mongoose for mongodd.

--- backend/project-vidtube/src/app.js
+++ backend/project-vidtube/src/app.js
@@ -13,20 +13,24 @@ app.use(
 
 
 // common middlewares.
-app.use(
-    express.json({limit: "16kb"})
-)
+app.use(express.json({limit: "16kb"}))
 
-app.use(
-    express.urlencoded({
+app.use(express.urlencoded({
         extended: true,
         limit: "16kb"
     })
 )
 
-app.use(
-    express.static("public")
-)
+app.use(express.static("public"))
+
+
+
+// importing routes 
+import healthCheckRouter from "./routes/healthcheck.routes.js"
+
+//routes 
+app.use("/api/v1/healthcheck", healthCheckRouter)
+
 
 
 export { app }
